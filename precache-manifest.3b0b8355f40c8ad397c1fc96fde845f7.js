4d93f5f9e7a274f85f9267c65216a44fef847b60
Lucile <developer.lvt@gmail.com>
2021-04-15 11:29:01 -0400

    Updates

---
+++ precache-manifest.3b0b8355f40c8ad397c1fc96fde845f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6568279a8daadb2cf5bd7042779158b3",
+    "url": "/quotes-generator-react/index.html"
+  },
+  {
+    "revision": "d59d6142a8ca2ce68114",
+    "url": "/quotes-generator-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "854e0cab860a46f158c8",
+    "url": "/quotes-generator-react/static/js/2.ffecf7dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotes-generator-react/static/js/2.ffecf7dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d59d6142a8ca2ce68114",
+    "url": "/quotes-generator-react/static/js/main.97bece44.chunk.js"
+  },
+  {
+    "revision": "3666a0116f897893738d",
+    "url": "/quotes-generator-react/static/js/runtime-main.6532651c.js"
+  }
+]);
